72a5e21f72ecc6af4ea4c6f1d79e2fdf5e370078
yago.pmc <yagopr95@gmail.com>
2022-10-12 14:06:38 -0300

    jokenpo

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+var Jogador1;
+var Jogador2;
+
+function Escolha1(){
+    Jogador1 = "1";
+}
+function Escolha2(){
+    Jogador1 = "2";
+}
+function Escolha3(){
+    Jogador1 = "3";
+}
+function Escolha4(){
+    Jogador2 = "4";
+}
+function Escolha5(){
+    Jogador2 = "5";
+}
+function Escolha6(){
+    Jogador2 = "6";
+}
+
